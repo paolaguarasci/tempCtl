108c95fbe6040e7deacd48898f87a6f1568fa243
Paola Guarasci <paolaguarasci@gmail.com>
2021-01-11 00:38:21 +0100

    rename and add log system

--- tempCtl.ps1
+++ tempCtl.ps1
@@ -1,10 +1,14 @@
-$JSONRequest = 'http://192.168.1.113:1380/getsupply'
-$JSONContent = Invoke-WebRequest $JSONRequest | ConvertFrom-Json 
-$temp = $JSONContent.supply
+# $JSONRequest = 'http://192.168.1.113:1380/getsupply'
+# $JSONContent = Invoke-WebRequest $JSONRequest | ConvertFrom-Json 
+# $volt = $JSONContent.supply
+$volt = 10;
 $limite = Get-Content "limite.txt"
-if ($temp -ge $limite) {
-  Write-Host 'Temperatura maggiore di ';
-  Write-Host $limite;
-  Write-Host '... spegnimento...';
-  Stop-Computer -ComputerName localhost;
+$data = Get-Date -Format "yyyyMMddHHmmss"
+$msg = $data + " " + $volt;
+Add-Content log.txt $msg
+# Write-EventLog -LogName "Application" -Source "VoltSh" -EventID 3001 -EntryType Information -Message "[LETTURA VOLT]" + $msg;
+if ($volt -le $limite) {
+  Add-Content log.txt "Spegnimento lettura volt inferiore al limite impostato" 
+  # Write-EventLog -LogName "Application" -Source "VoltSh" -EventID 3001 -EntryType Information -Message "Shutdown - lettura volt inferiore al limite impostato"
+  # Stop-Computer -ComputerName localhost;
 }
