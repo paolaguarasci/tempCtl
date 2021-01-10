e57cfc47874f3a3791a82787be6950e167895a1e
Paola Guarasci <paolaguarasci@gmail.com>
2021-01-11 00:39:06 +0100

    clean

--- voltSh.ps1
+++ voltSh.ps1
@@ -1,7 +1,6 @@
-# $JSONRequest = 'http://192.168.1.113:1380/getsupply'
-# $JSONContent = Invoke-WebRequest $JSONRequest | ConvertFrom-Json 
-# $volt = $JSONContent.supply
-$volt = 10;
+$JSONRequest = 'http://192.168.1.113:1380/getsupply'
+$JSONContent = Invoke-WebRequest $JSONRequest | ConvertFrom-Json 
+$volt = $JSONContent.supply
 $limite = Get-Content "limite.txt"
 $data = Get-Date -Format "yyyyMMddHHmmss"
 $msg = $data + " " + $volt;
@@ -10,5 +9,5 @@ Add-Content log.txt $msg
 if ($volt -le $limite) {
   Add-Content log.txt "Spegnimento lettura volt inferiore al limite impostato" 
   # Write-EventLog -LogName "Application" -Source "VoltSh" -EventID 3001 -EntryType Information -Message "Shutdown - lettura volt inferiore al limite impostato"
-  # Stop-Computer -ComputerName localhost;
+  Stop-Computer -ComputerName localhost;
 }
